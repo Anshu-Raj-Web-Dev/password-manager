4f24e9a776a9ff1525245c1581024344abb9cac4
Anshu-Raj-Web-Dev <157576222+Anshu-Raj-Web-Dev@users.noreply.github.com>
2024-11-17 09:47:41 +0530

    Update Manager.jsx

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,26 +1,23 @@
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import "./Manager.css"
+import "./Manager.css";
 
 const Manager = () => {
-
-    const ref = useRef()
-    const passwordRef = useRef()
-    const [form, setForm] = useState({ website: "", username: "", password: "" })
-    const [passwordArray, setPasswordArray] = useState([])
+    const ref = useRef();
+    const passwordRef = useRef();
+    const [form, setForm] = useState({ website: "", username: "", password: "" });
+    const [passwordArray, setPasswordArray] = useState([]);
     const [showPopup, setShowPopup] = useState(false);
-const [selectedPasswordId, setSelectedPasswordId] = useState(null);
-
+    const [selectedPasswordId, setSelectedPasswordId] = useState(null);
 
     useEffect(() => {
-        let passwords = localStorage.getItem("passwords")
+        let passwords = localStorage.getItem("passwords");
         if (passwords) {
-            setPasswordArray(JSON.parse(passwords))
+            setPasswordArray(JSON.parse(passwords));
         }
-    }, [])
-
+    }, []);
 
     const savePassword = () => {
         if (form.website && form.username && form.password) {
@@ -34,140 +31,131 @@ const [selectedPasswordId, setSelectedPasswordId] = useState(null);
     };
 
     const confirmDeletePassword = (id) => {
-    setShowPopup(true);
-    setSelectedPasswordId(id);
-};
+        setShowPopup(true);
+        setSelectedPasswordId(id);
+    };
 
     const deletePassword = () => {
-    const updatedPasswords = passwordArray.filter((item) => item.id !== selectedPasswordId);
-    setPasswordArray(updatedPasswords);
-    setShowPopup(false);
-    toast.info('Password deleted!', { position: 'top-left', autoClose: 1000 });
-};
-
+        const updatedPasswords = passwordArray.filter((item) => item.id !== selectedPasswordId);
+        setPasswordArray(updatedPasswords);
+        setShowPopup(false);
+        toast.info('Password deleted!', { position: 'top-left', autoClose: 1000 });
+    };
 
     const editPassword = (id) => {
-        setForm(passwordArray.filter(i => i.id === id)[0])
-        setPasswordArray(passwordArray.filter(item => item.id !== id))
-    }
+        setForm(passwordArray.filter((i) => i.id === id)[0]);
+        setPasswordArray(passwordArray.filter((item) => item.id !== id));
+    };
 
     const handleChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value })
-    }
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
 
     const showPassword = () => {
-        passwordRef.current.type = "text"
         if (ref.current.src.includes("icons/show-btn.png")) {
-            passwordRef.current.type = "text"
-            ref.current.src = "icons/hide-btn.png"
+            passwordRef.current.type = "text";
+            ref.current.src = "icons/hide-btn.png";
+        } else {
+            passwordRef.current.type = "password";
+            ref.current.src = "icons/show-btn.png";
         }
-        else {
-            ref.current.src = "icons/show-btn.png"
-            passwordRef.current.type = "password"
-        }
-    }
+    };
 
     const copyText = (text) => {
         navigator.clipboard.writeText(text);
         toast.info('Copied to clipboard!', { position: 'top-left', autoClose: 1000 });
     };
 
-
-
     return (
         <>
-        <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-            transition="Bounce"
-        />
-    <ToastContainer />
-        <div className={`manager-container ${showPopup ? 'blurred' : ''}`}>
-            <div className="title">
-                Your own Password Manager
-            </div>
+            {/* <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition="Bounce"
+            />
+            <ToastContainer /> */}
 
-            <div className='inputs'>
+            {/* Apply the blurred class to the main content */}
+            <div className={`manager-container ${showPopup ? 'blurred' : ''}`}>
+                <div className="title">Your own Password Manager</div>
 
-                <input value={form.website} onChange={handleChange} type="text" name='website' id='website' placeholder='Website' />
-
-                <div className="username-password">
-                    <input value={form.username} onChange={handleChange} type="text" name='username' id='username' placeholder='Username' />
-
-                    <div className="relative">
-                        <input value={form.password} onChange={handleChange} ref={passwordRef} type="password" name='password' id='password' placeholder='Password' />
-                        <span className='show-hide' onClick={showPassword}>
-                            <img ref={ref} src="icons/hide-btn.png" width={25} />
-                        </span>
+                <div className='inputs'>
+                    <input value={form.website} onChange={handleChange} type="text" name='website' placeholder='Website' />
+                    <div className="username-password">
+                        <input value={form.username} onChange={handleChange} type="text" name='username' placeholder='Username' />
+                        <div className="relative">
+                            <input value={form.password} onChange={handleChange} ref={passwordRef} type="password" name='password' placeholder='Password' />
+                            <span className='show-hide' onClick={showPassword}>
+                                <img ref={ref} src="icons/hide-btn.png" width={25} />
+                            </span>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <button className='save' onClick={savePassword} >Save Password</button>
+                <button className='save' onClick={savePassword}>Save Password</button>
 
-            <div className="your-passwords">
-                <h1 className='your-passwords-h1'>Your Passwords</h1>
-            </div>
-            {passwordArray.length === 0 && <div className='no-passwords'>No Passwords to show</div>}
-            {passwordArray.length != 0 &&
-                <div className="table">
-                    <div className="heading">
-                        <ul>
-                            <div className="site">Site</div>
-                            <div className="username">Username</div>
-                            <div className="password">Password</div>
-                            <div className="action">Actions</div>
-                        </ul>
-                    </div>
-                    <div className="saved-passwords">
-                        <ul>
-                            {passwordArray.map((item, index) => {
+                <div className="your-passwords">
+                    <h1 className='your-passwords-h1'>Your Passwords</h1>
+                </div>
 
-                                return <li className='saved-password-list' key={index}>
+                {passwordArray.length === 0 && <div className='no-passwords'>No Passwords to show</div>}
 
-                                    
-                                        <div className="saved-website"><a href={item.website} target='_blank'> {item.website}</a></div>
+                {passwordArray.length !== 0 && (
+                    <div className="table">
+                        <div className="heading">
+                            <ul>
+                                <div className="site">Site</div>
+                                <div className="username">Username</div>
+                                <div className="password">Password</div>
+                                <div className="action">Actions</div>
+                            </ul>
+                        </div>
+                        <div className="saved-passwords">
+                            <ul>
+                                {passwordArray.map((item, index) => (
+                                    <li className='saved-password-list' key={index}>
+                                        <div className="saved-website">
+                                            <a href={item.website} target='_blank'> {item.website}</a>
+                                        </div>
                                         <div className="saved-username">{item.username}</div>
                                         <div className="saved-password">{item.password}</div>
                                         <div className="saved-action">
                                             <div className="edit" onClick={() => { editPassword(item.id) }}>
-                                                <img src="icons/edit.png"/>
+                                                <img src="icons/edit.png" />
                                             </div>
-                                            <div className="delete" onClick={() => { deletePassword(item.id) }}>
-                                                <img src="icons/delete.png" width={25}/>
+                                            <div className="delete" onClick={() => { confirmDeletePassword(item.id) }}>
+                                                <img src="icons/delete.png" width={25} />
                                             </div>
                                             <div className="copy" onClick={() => { copyText(item.website) }}>
-                                            <img src="icons/copy.png"/>
+                                                <img src="icons/copy.png" />
                                             </div>
                                         </div>
-                                    
-
-                                </li>
-                            })}
-
-                        </ul>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
-                </div>}
-            
-)}
+                )}
+            </div>
 
-        </div>
+            {/* Popup */}
             {showPopup && (
-    <div className="popup">
-        <h1>Are you sure you want to delete this password?</h1>
-        <button className="yes" onClick={deletePassword}>Yes</button>
-        <button className="cancel" onClick={() => setShowPopup(false)}>Cancel</button>
-    </div>
-            </>
-    )
-}
+                <div className="popup">
+                    <h1>Are you sure you want to delete this password?</h1>
+                    <button className="yes" onClick={deletePassword}>Yes</button>
+                    <button className="cancel" onClick={() => setShowPopup(false)}>Cancel</button>
+                </div>
+            )}
+        </>
+    );
+};
 
-export default Manager
+export default Manager;
